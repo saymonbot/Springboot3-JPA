d26736d382b916bf0bb0a21ab80d43117615deac
Saymon Botelho <saymon_botelho123@outlook.com>
2024-01-16 11:55:39 -0300

    Payment, one-to-one association

--- src/main/java/com/saymon/course/config/TestConfig.java
+++ src/main/java/com/saymon/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.saymon.course.entities.Category;
 import com.saymon.course.entities.Order;
 import com.saymon.course.entities.OrderItem;
+import com.saymon.course.entities.Payment;
 import com.saymon.course.entities.Product;
 import com.saymon.course.entities.User;
 import com.saymon.course.entities.enums.OrderStatus;
@@ -81,6 +82,9 @@ public class TestConfig implements CommandLineRunner {
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
 		
+		orderRepository.save(o1);
 	}
 }
