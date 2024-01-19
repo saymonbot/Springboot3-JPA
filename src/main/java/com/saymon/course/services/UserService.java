724a1a30f65c0a8f3b96509cb8b2fab80ca82bfa
Saymon Botelho <saymon_botelho123@outlook.com>
2024-01-18 21:17:27 -0300

    Exception handling - delete

--- src/main/java/com/saymon/course/services/UserService.java
+++ src/main/java/com/saymon/course/services/UserService.java
@@ -3,12 +3,14 @@ package com.saymon.course.services;
 import java.util.List;
 import java.util.Optional;
 
-import org.hibernate.ResourceClosedException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.saymon.course.entities.User;
 import com.saymon.course.repositories.UserRepository;
+import com.saymon.course.services.exceptions.DatabaseException;
 import com.saymon.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -31,7 +33,13 @@ public class UserService {
 	}
 
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e){
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e){
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj) {
