98dcc987241ef1f1af23b88b30b61fcdbe61a7b6
Saymon Botelho <saymon_botelho123@outlook.com>
2024-01-18 21:29:01 -0300

    Exception handling - update

--- src/main/java/com/saymon/course/services/UserService.java
+++ src/main/java/com/saymon/course/services/UserService.java
@@ -3,6 +3,7 @@ package com.saymon.course.services;
 import java.util.List;
 import java.util.Optional;
 
+import org.hibernate.internal.build.AllowPrintStacktrace;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -12,6 +13,8 @@ import com.saymon.course.entities.User;
 import com.saymon.course.repositories.UserRepository;
 import com.saymon.course.services.exceptions.DatabaseException;
 import com.saymon.course.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class UserService {
@@ -43,10 +46,13 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
-		
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}	
 	}
 
 	private void updateData(User entity, User obj) {
